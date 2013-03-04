8a7a58de63932d94c79e14250d1e0767aefe1e7a
Andy Zhang <andy.zhang@intel.com>
2013-03-04 17:23:13 -0500

    Support libcilkrts and cilk headers with autoconf

--- make/platform/clang_linux.mk
+++ make/platform/clang_linux.mk
@@ -63,7 +63,7 @@ endif
 # Build runtime libraries for x86_64.
 ifeq ($(call contains,$(SupportedArches),x86_64),true)
 Configs += full-x86_64 profile-x86_64 san-x86_64 asan-x86_64 tsan-x86_64 \
-           msan-x86_64 ubsan-x86_64 ubsan_cxx-x86_64
+           msan-x86_64 ubsan-x86_64 ubsan_cxx-x86_64 cilk-x86_64
 Arch.full-x86_64 := x86_64
 Arch.profile-x86_64 := x86_64
 Arch.san-x86_64 := x86_64
@@ -72,6 +72,7 @@ Arch.tsan-x86_64 := x86_64
 Arch.msan-x86_64 := x86_64
 Arch.ubsan-x86_64 := x86_64
 Arch.ubsan_cxx-x86_64 := x86_64
+Arch.cilk-x86_64 := x86_64
 endif
 
 ifneq ($(LLVM_ANDROID_TOOLCHAIN_DIR),)
@@ -102,8 +103,16 @@ CFLAGS.ubsan-i386 := $(CFLAGS) -m32 -fPIE -fno-builtin -fno-rtti
 CFLAGS.ubsan-x86_64 := $(CFLAGS) -m64 -fPIE -fno-builtin -fno-rtti
 CFLAGS.ubsan_cxx-i386 := $(CFLAGS) -m32 -fPIE -fno-builtin
 CFLAGS.ubsan_cxx-x86_64 := $(CFLAGS) -m64 -fPIE -fno-builtin
+CFLAGS.cilk-x86_64 := -O3 -m64 -fcilkplus -fPIC \
+                      -I$(ProjSrcRoot)/lib/cilk/include \
+                      -D_Cilk_spawn= -D_Cilk_sync= -D_Cilk_for=for \
+                      -D__CILKRTS_ABI_VERSION=1 -DIN_CILK_RUNTIME=1 \
+                      -DBUILD_HOST=\"Linux\" -DBUILD_USER=\"anonymous\"
 
 SHARED_LIBRARY.asan-arm-android := 1
+SHARED_LIBRARY.cilk-x86_64 := 1
+LD.cilk-x86_64 := $(CXX)
+
 ANDROID_COMMON_FLAGS := -target arm-linux-androideabi \
 	--sysroot=$(LLVM_ANDROID_TOOLCHAIN_DIR)/sysroot \
 	-B$(LLVM_ANDROID_TOOLCHAIN_DIR)
@@ -140,6 +149,9 @@ FUNCTIONS.ubsan-i386 := $(UbsanFunctions)
 FUNCTIONS.ubsan-x86_64 := $(UbsanFunctions)
 FUNCTIONS.ubsan_cxx-i386 := $(UbsanCXXFunctions)
 FUNCTIONS.ubsan_cxx-x86_64 := $(UbsanCXXFunctions)
+FUNCTIONS.cilk-x86_64 := $(CilkrtsFunctions)
+
+Headers.cilk-x86_64 := $(CilkHeaders)
 
 # Always use optimized variants.
 OPTIMIZED := 1
